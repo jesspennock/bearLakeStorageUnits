1c72b179df1d2f019511c9a6fb2b947cdda6d3cf
Jess Pennock <jess.pennock@gmail.com>
2022-10-31 14:03:37 -0600

    modal designs

--- client/main.js
+++ client/main.js
@@ -2,7 +2,7 @@ const unitDetailsBtnMed = document.getElementById("typeInfoMed")
 const unitDetailsBtnLarge = document.getElementById("typeInfoLarge")
 const unitDetailsBtnXLarge = document.getElementById("typeInfoXLarge")
 const unitDetailsBtnFlex = document.getElementById("typeInfoFlex")
-const getAllUnitsModal = document.getElementById("allUnits")
+const unitTableBody = document.getElementById("unitTableBody")
 
 const errCallback = err => console.log(err)
 
@@ -15,6 +15,7 @@ const getAllUnits = () => {
         console.log(res)
         // check if all available === false -> show banner or something
         // add all unit elements to the "get all unit info" modal
+        // call loadUnitTable with the response data
     })
     .catch(errCallback)
 }
@@ -44,14 +45,19 @@ const addWaiter = () => {
 }
 
 const submitNewReview = () => {
-    
+
+}
+
+const loadUnitTable = (data) => {
+    // iterate over the data
+    // create a new <tr> for each
+    // build out the columns in that row with the data
+    // append it to the unitTableBody
 }
 
 unitDetailsBtnMed.addEventListener('click', () => getUnitSizeGroup('10x24'));
 unitDetailsBtnLarge.addEventListener('click', () => getUnitSizeGroup('12x32'));
 unitDetailsBtnXLarge.addEventListener('click', () => getUnitSizeGroup('12x40'));
 unitDetailsBtnFlex.addEventListener('click', () => getUnitSizeGroup('45x45'));
-
-
 
 getAllUnits()
